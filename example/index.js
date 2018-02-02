8cb9a1843183ec30e078550a7207b142d9f7d76c
Matthew Drake <mdrake@mediadrake.com>
2018-02-02 15:13:44 -0800

    added example

--- example/index.js
+++ example/index.js
@@ -1,7 +1,9 @@
 import wt from '../src';
 
 wt('initialize', {
-  trackerUrl: 'http://localhost:8080/track.gif',
+  trackerUrl: 'http://localhost:3000/track.gif',
 });
+
+wt('pageview');
 
 // const buttonOne = document.findElementById('button-one');
